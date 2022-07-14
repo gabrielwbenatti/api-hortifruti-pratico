45446040915666207fd8d72d7ea2a191e7af965f
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-14 12:04:05 -0300

    removido routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,38 +1,7 @@
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-
 import Route from "@ioc:Adonis/Core/Route";
-import User from "App/Models/User";
 
 Route.get("/", async () => {
-  User.create({
-    email: "admin@email.com",
-    password: "123456",
-    tipo: "admin",
-  });
-});
-
-Route.post(
-  "/gettoken",
-  async ({ request, response, auth }: HttpContextContract) => {
-    const email = request.input("email");
-    const password = request.input("password");
-
-    const user = await User.findBy("email", email);
-
-    if (user == null) {
-      return response.notFound("Usuário não encontradp");
-    }
-
-    const token = await auth.use("api").attempt(email, password);
-
-    return response.ok(token);
-  }
-);
-
-Route.get("/auth", async ({ auth, response }: HttpContextContract) => {
-  const userAuth = await auth.use("api").authenticate();
-
-  if (userAuth) {
-    return response.ok(userAuth);
-  }
+  return {
+    hortifruti: "pratico",
+  };
 });
