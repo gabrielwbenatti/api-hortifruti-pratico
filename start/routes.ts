615c7e88270a2a34b245c3d597043ff151f8c6a0
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-13 21:45:40 -0300

    ajustes nas migrations
    timestamps cria os campos "created_at" e "updated_at"

--- start/routes.ts
+++ start/routes.ts
@@ -1,6 +1,10 @@
+import Route from "@ioc:Adonis/Core/Route";
+// import User from "App/Models/User";
 
-import Route from '@ioc:Adonis/Core/Route'
-
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get("/", async () => {
+  // User.create({
+  //   email: "admin@email.com",
+  //   password: "123456",
+  //   tipo: "admin",
+  // });
+});
