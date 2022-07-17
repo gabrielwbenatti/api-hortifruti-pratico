afc1ca356036e952968bac7099f19a839dd1dd14
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-16 21:53:28 -0300

    add relacionamento

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,12 @@ Route.post("/logout", "AuthController.logout");
 
 Route.post("/cliente/cadastro", "ClientesController.store");
 
+Route.get("/cidades", "CidadesController.index");
+Route.get(
+  "/cidades/:id/estabelecimentos",
+  "CidadesController.Estabelecimentos"
+);
+
 Route.group(() => {
   Route.get("auth/me", "AuthController.me");
 
