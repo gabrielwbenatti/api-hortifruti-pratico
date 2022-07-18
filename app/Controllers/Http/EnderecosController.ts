a82807cb72baf832afd22a8d251100f36dc5d343
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-17 21:55:14 -0300

    add put e delete de endereço

--- app/Controllers/Http/EnderecosController.ts
+++ app/Controllers/Http/EnderecosController.ts
@@ -5,6 +5,31 @@ import Endereco from "App/Models/Endereco";
 import CreateEditEnderecoValidator from "App/Validators/CreateEditEnderecoValidator";
 
 export default class EnderecosController {
+  public async destroy({ response, params }: HttpContextContract) {
+    try {
+      const resp = await Endereco.query().where("id", params.id).delete();
+
+      if (resp.includes(1)) {
+        return response.noContent();
+      } else {
+        return response.notFound();
+      }
+    } catch (error) {
+      return response.badRequest();
+    }
+  }
+
+  public async update({ request, response, params }: HttpContextContract) {
+    const payload = await request.validate(CreateEditEnderecoValidator);
+    const endereco = await Endereco.findByOrFail("id", params.id);
+
+    endereco.merge(payload);
+
+    await endereco.save();
+
+    return response.ok(endereco);
+  }
+
   public async store({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(CreateEditEnderecoValidator);
     const userAuth = await auth.use("api").authenticate();
