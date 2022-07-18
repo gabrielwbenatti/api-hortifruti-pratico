912ddd19fd855ccea69119aca35346177dbf0505
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-17 22:30:20 -0300

    Update 1658106667148_pedidos_statuses.ts

--- database/migrations/1658106667148_pedidos_statuses.ts
+++ database/migrations/1658106667148_pedidos_statuses.ts
@@ -5,7 +5,6 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments("id").primary();
       table
         .integer("pedido_id")
         .notNullable()
