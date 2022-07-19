77894473fc1d70da190b4b80cfba9b1d74717f90
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-19 11:39:11 -0300

    rename

--- app/Models/EstabelecimentoMeiosPagamento.ts
+++ app/Models/EstabelecimentoMeiosPagamento.ts
@@ -1,6 +1,6 @@
 import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
 
-export default class EstebelecimentoMeiosPagamento extends BaseModel {
+export default class EstabelecimentoMeiosPagamento extends BaseModel {
   @column({ isPrimary: true })
   public estabelecimento_id: number;
 
